bf67ec18d5df2c73f77cdeb6ecef3f9da864f6d1
Vasily Belolapotkov <belolapotkov.v@gmail.com>
2016-06-20 10:51:29 +0300

    Added form submit handler

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
   setupScrollSpy();
   $(window).scroll(handleBodyScroll);
   $('.local-nav-link').on('click', handleLocalNavLinkClick);
+  $('#contactForm').submit(submitContact);
 });
 
 function setupScrollSpy() {
@@ -55,4 +56,72 @@ function scrollTo(selector) {
   var offset = SCROLL_OFFSET;
   var scrollTop = elementTop - offset;
   $('body').animate({"scrollTop": scrollTop}, 'slow');
+}
+
+function submitContact(e) {
+  var form = e.currentTarget;
+  try {
+    var contact = getFormData(form);
+    checkContact(contact);
+    clearForm(form);
+  } catch (error) {
+    handleSubmitContactError(error, form);
+    e.preventDefault();
+  }
+}
+
+function getFormData(form) {
+  var data = {};
+  var valuesArray = $(form).serializeArray();
+  valuesArray.forEach(function(value) {
+    data[value.name] = value.value;
+  });
+  return data;
+}
+
+function checkContact(data) {
+  if (!data[ "Name" ] || !data[ "Contact" ]) {
+    throw new Error('EMPTY_CONTACT_FIELD');
+  }
+}
+
+function clearForm(form) {
+  clearValues(form);
+  clearErrorState(form);
+}
+
+function clearValues(form) {
+  $(form).find('input').val('');
+}
+
+function handleSubmitContactError(error, form) {
+  clearErrorState(form);
+  if (error.message === 'EMPTY_CONTACT_FIELD') {
+    notifyUserOnEmptyFields(form);
+  } else {
+    notifyUserOnUnknownError(form);
+  }
+}
+
+function clearErrorState(form) {
+  $(form).children('.form-group').removeClass('has-error');
+  $(form).find('.error-msg').detach();
+}
+
+function notifyUserOnEmptyFields(form) {
+  $(form).children('.form-group').addClass('has-error');
+  var errorMsg = composeErrorMsg('Please fill the form to let me know how to reach you.');
+  $(form).prepend(errorMsg);
+}
+
+function composeErrorMsg(msg) {
+  var msgContainer = document.createElement('div');
+  msgContainer.className = 'row alert alert-danger error-msg';
+  msgContainer.innerHTML = msg;
+  return msgContainer;
+}
+
+function notifyUserOnUnknownError(form) {
+  var errorMsg = composeErrorMsg('Ooops, something unexpected happend. Anyway you can contact me using links above this message.');
+  $(form).prepend(errorMsg);
 }

fc80208d02ec7fc7a146371c32804995c93633d6
Vasily Belolapotkov <belolapotkov.v@gmail.com>
2018-12-29 13:15:01 +0300

    replace page with redirect message

--- src/js/index.js
+++
@@ -1,7 +0,0 @@
-import setupNav from './nav';
-import setupContactForm from './contact-form';
-
-$(document).ready(function() {
-  setupNav();
-  setupContactForm();
-});
--- src/js/nav/index.js
+++
@@ -1,8 +0,0 @@
-import setupScroll from './scroll';
-import setupNav from './nav';
-
-export default function() {
-  const scrollOffset = 30;
-  setupScroll(scrollOffset);
-  setupNav(scrollOffset);
-}
